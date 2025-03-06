684b6bd9b6f1786e805878e586955953cd0acc0c
yujung <yujung7439@gmail.com>
2025-03-07 02:41:04 +0900

    [Feature39] 메인 화면에 topbar와 리스트<->그리드로 변환해주는 아이콘 추가

--- app/src/main/java/com/example/objectdetection/activity/MainComposeActivity.kt
+++ app/src/main/java/com/example/objectdetection/activity/MainComposeActivity.kt
@@ -3,27 +3,72 @@ package com.example.objectdetection.activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import com.example.objectdetection.MainViewModel
+import com.example.objectdetection.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainComposeActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            ComposeScreen()
+            ComposeMainScreen(viewModel)
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ComposeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize()
+fun ComposeMainScreen(viewModel: MainViewModel) {
+    Column(
+        modifier = Modifier.background(Color.White)
     ) {
-        Text("This is the Compose Activity")
+        var isSelected by rememberSaveable { mutableStateOf(false) }
+
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    textAlign = TextAlign.Center,
+                    fontWeight = FontWeight.Bold
+                )
+            },
+            actions = {
+                IconButton(onClick = {
+                    isSelected = !isSelected
+                }) {
+                    Icon(
+                        painter = if (isSelected) {
+                            painterResource(id = R.drawable.ic_list_24)
+                        } else {
+                            painterResource(id = R.drawable.ic_grid_24)
+                        },
+                        contentDescription = "List"
+                    )
+                }
+            }
+        )
     }
 }
